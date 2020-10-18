b6301981ac8f3266c80f8083283a759863540402
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-18 22:09:00 +0530

    Removed default state

--- src/app/services/state/state.ts
+++ src/app/services/state/state.ts
@@ -6,65 +6,7 @@ export class State {
   static state: BehaviorSubject<GlobalStateInterface> = new BehaviorSubject<
     GlobalStateInterface
   >({
-    questions: {
-      abcd: {
-        title: "Hello World! 1",
-        content:
-          "Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question ",
-        votes: {
-          total: 1,
-          upVoters: ["qwertyuiopasdfghjl"],
-          downVoters: [],
-        },
-      },
-      efgh: {
-        title: "Hello World! 2",
-        content: "Hey this is my second question",
-        votes: {
-          total: -1,
-          upVoters: [],
-          downVoters: ["qwertyuiopasdfghjl"],
-        },
-      },
-      abcd2: {
-        title: "Hello World! 1",
-        content:
-          "Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question ",
-        votes: {
-          total: 1,
-          upVoters: ["qwertyuiopasdfghjl"],
-          downVoters: [],
-        },
-      },
-      efgh2: {
-        title: "Hello World! 2",
-        content: "Hey this is my second question",
-        votes: {
-          total: -1,
-          upVoters: [],
-          downVoters: ["qwertyuiopasdfghjl"],
-        },
-      },
-      abcd3: {
-        title: "Hello World! 1",
-        content:
-          "Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question Hey this is my first question ",
-        votes: {
-          total: 1,
-          upVoters: ["qwertyuiopasdfghjl"],
-          downVoters: [],
-        },
-      },
-      efgh3: {
-        title: "Hello World! 2",
-        content: "Hey this is my second question",
-        votes: {
-          total: -1,
-          upVoters: [],
-          downVoters: ["qwertyuiopasdfghjl"],
-        },
-      },
-    },
+    questions: {},
     tags: [
       {
         name: "C++",
