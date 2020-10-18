ae963c92a798c28419a25ccde26d54a706e89d3c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-18 21:39:27 +0530

    Minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./app/App";
-import "./index.scss";
+import "./app/styles/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
